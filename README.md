ef5cc6db770f37af1d0684ecd1c6d88501ff0bc7
jaxabercrombie <jabercro@usc.edu>
2021-12-09 18:19:09 -0800

    Finishing website

---
+++ README.md
@@ -0,0 +1,19 @@
+Project Title:
+Investigating California's COVID-19 Vaccination Efforts by Region and County
+
+
+Brief Description:
+This project investigates how California is handling COVID-19 vaccines by region 
+and/or county, focusing on questions related to: (1) percent of fully vaccinated 
+individuals by county/region, (2) how increases in cases/deaths potentially
+influence vaccine uptake, (3) how vaccine companies differ in share of the 
+market, (4) how vaccination efforts differ by age group, and (5) how vaccination 
+efforts differ by race and ethnicity.
+
+
+Link to my website:
+https://jaxabercrombie.github.io/pm566-finalproject/ 
+
+
+Link to my repository for this project: 
+https://github.com/jaxabercrombie/pm566-finalproject 
--- datawrangle.R
+++ datawrangle.R
@@ -50,3 +50,52 @@ datawrangle <- function(category) {
   
   return(demoCA)
 }
+
+datawrangle2 <- function(category) {
+  # Region categories to sort counties
+  superior <- c("Butte", "Colusa", "Del Norte", "Glenn", "Humboldt", "Lake", 
+                "Lassen", "Mendocino", "Modoc", "Nevada", "Plumas", "Shasta", 
+                "Sierra", "Siskiyou", "Tehama", "Trinity")
+  central <- c("Alpine", "Amador", "Calaveras", "El Dorado", "Fresno", "Inyo", 
+               "Kings", "Madera", "Mariposa", "Merced", "Mono", "Placer", 
+               "Sacramento", "San Joaquin", "Stanislaus", "Sutter", "Yuba", 
+               "Tulare", "Tuolumne", "Yolo")
+  bay <- c("Alameda", "Contra Costa", "Marin", "Monterey", "Napa", "San Benito", 
+           "San Francisco", "San Mateo", "Santa Clara", "Santa Cruz", "Solano", 
+           "Sonoma")
+  southern <- c("Imperial", "Kern", "Orange", "Riverside", "San Bernardino", 
+                "San Diego", "San Luis Obispo", "Santa Barbara", "Ventura", 
+                "Los Angeles")
+  
+  download3 <- getURL("https://data.lacity.org/api/views/iv7a-6rrq/rows.csv?accessType=DOWNLOAD")
+  demoCA <- read.csv(text = download3)
+  
+  
+  
+  demoCA <- 
+    demoCA %>%
+    filter(date == "10/31/2021 12:00:00 AM") %>%
+    filter(demographic_category == category) %>%
+    mutate(region = case_when(county %in% superior ~ "Superior",
+                              county %in% central ~ "Central",
+                              county %in% bay ~ "Bay Area",
+                              county %in% southern ~ "Southern")) %>%
+    summarise(date = date,
+              region = region,
+              county = county,
+              demographic_category = demographic_category,
+              demographic_value = demographic_value,
+              perc_fully_vax = (cumulative_fully_vaccinated/est_population))
+  
+  demoCA <- na.omit(demoCA)
+  
+  demoCA <-
+    demoCA %>%
+    group_by(region, demographic_value) %>%
+    summarise(region = region,
+              county = county,
+              demographic_value = demographic_value,
+              perc_fully_vax = perc_fully_vax*100)
+  
+  return(demoCA)
+}
